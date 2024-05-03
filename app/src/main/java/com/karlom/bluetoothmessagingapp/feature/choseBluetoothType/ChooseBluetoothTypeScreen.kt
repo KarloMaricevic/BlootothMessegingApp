ef0e00d0d0ef64dc93b122802cf1bf029ff09d46
Karlo Maricevic <karlomaricevic@gmail.com>
2024-05-03 14:25:20 +0200

    Prompt user to turn on bluetooth when he wants to search bluetooth devices

--- app/src/main/java/com/karlom/bluetoothmessagingapp/feature/choseBluetoothType/ChooseBluetoothTypeScreen.kt
+++ app/src/main/java/com/karlom/bluetoothmessagingapp/feature/choseBluetoothType/ChooseBluetoothTypeScreen.kt
@@ -1,7 +1,12 @@
 package com.karlom.bluetoothmessagingapp.feature.choseBluetoothType
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -9,7 +14,17 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun ChooseBluetoothTypeScreen() {
+    val bluetoothController = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult(),
+        onResult = { result -> }
+    )
     Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Hello!", Modifier.align(Alignment.Center))
+        Button(
+            onClick = {
+                val enableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                bluetoothController.launch(enableIntent)
+            },
+            modifier = Modifier.align(Alignment.Center)
+        ) { Text(text = "Start searching") }
     }
 }
