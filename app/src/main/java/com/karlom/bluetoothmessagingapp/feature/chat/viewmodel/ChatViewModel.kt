77c168d7439e1d84b71c50d9b3f98e86e7c5dcd8
Karlo Maricevic <karlomaricevic@gmail.com>
2024-05-28 16:54:25 +0200

    Send image to other device when user picks image from gallery

--- app/src/main/java/com/karlom/bluetoothmessagingapp/feature/chat/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/karlom/bluetoothmessagingapp/feature/chat/viewmodel/ChatViewModel.kt
@@ -3,17 +3,23 @@ package com.karlom.bluetoothmessagingapp.feature.chat.viewmodel
 import androidx.lifecycle.viewModelScope
 import com.karlom.bluetoothmessagingapp.core.base.BaseViewModel
 import com.karlom.bluetoothmessagingapp.core.base.TIMEOUT_DELAY
+import com.karlom.bluetoothmessagingapp.core.di.IoDispatcher
 import com.karlom.bluetoothmessagingapp.data.chat.ChatRepository
 import com.karlom.bluetoothmessagingapp.domain.bluetooth.usecase.IsConnectedToDevice
 import com.karlom.bluetoothmessagingapp.domain.chat.usecase.GetMessages
+import com.karlom.bluetoothmessagingapp.domain.chat.usecase.SendImage
 import com.karlom.bluetoothmessagingapp.domain.chat.usecase.SendMessage
 import com.karlom.bluetoothmessagingapp.feature.chat.models.ChatScreenEvent
-import com.karlom.bluetoothmessagingapp.feature.chat.models.ChatScreenEvent.*
+import com.karlom.bluetoothmessagingapp.feature.chat.models.ChatScreenEvent.OnConnectClicked
+import com.karlom.bluetoothmessagingapp.feature.chat.models.ChatScreenEvent.OnSendClicked
+import com.karlom.bluetoothmessagingapp.feature.chat.models.ChatScreenEvent.OnSendImageClicked
+import com.karlom.bluetoothmessagingapp.feature.chat.models.ChatScreenEvent.OnTextChanged
 import com.karlom.bluetoothmessagingapp.feature.chat.models.ChatScreenState
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
@@ -28,6 +34,8 @@ class ChatViewModel @AssistedInject constructor(
     private val sendMessage: SendMessage,
     private val mesRepository: ChatRepository,
     private val isConnectedToDevice: IsConnectedToDevice,
+    private val sendImage: SendImage,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : BaseViewModel<ChatScreenEvent>() {
 
     private val showConnectToDeviceButton = MutableStateFlow(!isConnectedToDevice(contactAddress))
@@ -54,7 +62,7 @@ class ChatViewModel @AssistedInject constructor(
             is OnTextChanged -> textToSend.update { event.text }
             is OnSendClicked -> viewModelScope.launch { sendMessage(textToSend.value) }
             is OnConnectClicked -> {}
-            is OnSendImageClicked -> {}
+            is OnSendImageClicked -> viewModelScope.launch(ioDispatcher) { sendImage(event.uri) }
         }
     }
 
