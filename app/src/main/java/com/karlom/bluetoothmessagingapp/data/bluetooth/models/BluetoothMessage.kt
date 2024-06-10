405a90d8281e076eef3e058d767727eb71621d28
Karlo Maricevic <karlomaricevic@gmail.com>
2024-06-10 13:02:19 +0200

    Make every child of Bluetooth message data class

--- app/src/main/java/com/karlom/bluetoothmessagingapp/data/bluetooth/models/BluetoothMessage.kt
+++ app/src/main/java/com/karlom/bluetoothmessagingapp/data/bluetooth/models/BluetoothMessage.kt
@@ -1,19 +1,55 @@
 package com.karlom.bluetoothmessagingapp.data.bluetooth.models
 
-sealed class BluetoothMessage(val address: String) {
+sealed class BluetoothMessage(open val address: String) {
 
-    class Text(
-        address: String,
+    data class Text(
+        override val address: String,
         val text: String
     ) : BluetoothMessage(address)
 
-    class Image(
-        address: String,
+    data class Image(
+        override val address: String,
         val image: ByteArray
-    ) : BluetoothMessage(address)
+    ) : BluetoothMessage(address) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
 
-    class Audio(
-        address: String,
+            other as Image
+
+            if (address != other.address) return false
+            if (!image.contentEquals(other.image)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = address.hashCode()
+            result = 31 * result + image.contentHashCode()
+            return result
+        }
+    }
+
+    data class Audio(
+        override val address: String,
         val audio: ByteArray
-    ) : BluetoothMessage(address)
+    ) : BluetoothMessage(address) {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Audio
+
+            if (address != other.address) return false
+            if (!audio.contentEquals(other.audio)) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = address.hashCode()
+            result = 31 * result + audio.contentHashCode()
+            return result
+        }
+    }
 }
