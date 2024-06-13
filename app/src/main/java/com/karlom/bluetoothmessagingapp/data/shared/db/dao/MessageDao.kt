6823fbc8462af2f93e8c77a18c9bf6f1b3351e7c
Karlo Maricevic <karlomaricevic@gmail.com>
2024-06-13 15:42:29 +0200

    Add way to get auto generated id after inserting messaging entity to db
    + add update method for message entity

--- app/src/main/java/com/karlom/bluetoothmessagingapp/data/shared/db/dao/MessageDao.kt
+++ app/src/main/java/com/karlom/bluetoothmessagingapp/data/shared/db/dao/MessageDao.kt
@@ -4,13 +4,20 @@ import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.karlom.bluetoothmessagingapp.data.shared.db.enteties.MessageEntity
 
 @Dao
 interface MessageDao {
 
     @Insert
+    suspend fun insert(message: MessageEntity): Long
+
+    @Insert
     suspend fun insertAll(vararg messages: MessageEntity)
+
+    @Update
+    suspend fun update(message: MessageEntity)
 
     @Query("SELECT * FROM MessageEntity WHERE withContactAddress = :withContactAddress ORDER BY id ASC")
     fun getMessages(withContactAddress: String): PagingSource<Int, MessageEntity>
