31419492110e7d0fa869ea41bfc746b8e9ffbdd4
Karlo Maricevic <karlomaricevic@gmail.com>
2024-06-14 15:45:38 +0200

    Add title to contact screen

--- app/src/main/java/com/karlom/bluetoothmessagingapp/feature/contacts/ContactsScreen.kt
+++ app/src/main/java/com/karlom/bluetoothmessagingapp/feature/contacts/ContactsScreen.kt
@@ -2,14 +2,18 @@ package com.karlom.bluetoothmessagingapp.feature.contacts
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.karlom.bluetoothmessagingapp.R
 import com.karlom.bluetoothmessagingapp.feature.contacts.components.Contact
 import com.karlom.bluetoothmessagingapp.feature.contacts.components.NewChatFloatingButton
 import com.karlom.bluetoothmessagingapp.feature.contacts.models.ContactScreenEvent.OnAddContactClicked
@@ -23,20 +27,27 @@ fun ContactsScreen(
 ) {
     val contacts = viewModel.contacts.collectAsLazyPagingItems()
 
-    Box {
-        SimpleLazyColumn(
-            items = contacts,
-            key = { contact.address },
-            uiItemBuilder = { contactUi ->
-                Contact(
-                    model = contactUi,
-                    modifier = Modifier.clickable {
-                        viewModel.onEvent(OnContactClicked(contactUi.contact.address))
-                    })
-            },
-            noItemsItem = { },
-            modifier = Modifier.fillMaxSize(),
-        )
+    Box(Modifier.padding(top = 8.dp)) {
+        Column {
+            Text(
+                text = stringResource(id = R.string.contacts_screen_chat_title),
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(start = 8.dp),
+            )
+            SimpleLazyColumn(
+                items = contacts,
+                key = { contact.address },
+                uiItemBuilder = { contactUi ->
+                    Contact(
+                        model = contactUi,
+                        modifier = Modifier.clickable {
+                            viewModel.onEvent(OnContactClicked(contactUi.contact.address))
+                        })
+                },
+                noItemsItem = { },
+                modifier = Modifier.weight(weight = 1f, fill = true),
+            )
+        }
         NewChatFloatingButton(
             onClick = { viewModel.onEvent(OnAddContactClicked) },
             modifier = Modifier
