c8d61a61d85ebac73fd6abd6431b638450be4995
Karlo Maricevic <karlomaricevic@gmail.com>
2024-06-22 18:40:28 +0200

    Fix UI of contact item in contact screen

--- app/src/main/java/com/karlom/bluetoothmessagingapp/feature/contacts/components/Contact.kt
+++ app/src/main/java/com/karlom/bluetoothmessagingapp/feature/contacts/components/Contact.kt
@@ -1,25 +1,24 @@
 package com.karlom.bluetoothmessagingapp.feature.contacts.components
 
-import androidx.compose.foundation.background
-
-import androidx.compose.foundation.border
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.karlom.bluetoothmessagingapp.designSystem.theme.BluetoothMessagingAppTheme
-import com.karlom.bluetoothmessagingapp.designSystem.theme.gray300
 import com.karlom.bluetoothmessagingapp.domain.contacts.models.Contact
 import com.karlom.bluetoothmessagingapp.feature.contacts.models.ContactUi
 
@@ -34,26 +33,46 @@ fun Contact(
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        Box(
-            Modifier
-                .clip(CircleShape)
-                .border(1.dp, gray300, CircleShape)
-                .background(color = model.color)
-                .padding(20.dp),
+        CircleWithLetter(model.contact.name.firstOrNull()?.uppercase() ?: "?")
+        Column(
+            Modifier.padding(
+                start = 8.dp,
+                end = 16.dp,
+            )
         ) {
-            Text(text = model.contact.name.firstOrNull()?.toString() ?: "?")
-        }
-        Column(Modifier.padding(horizontal = 16.dp)) {
             Text(
                 text = model.contact.name,
                 maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
             )
             Text(
                 text = model.lastMessage,
                 maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.bodySmall,
             )
         }
+    }
+}
+
+@Composable
+private fun CircleWithLetter(letter: String) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(40.dp)
+    ) {
+        Canvas(modifier = Modifier.matchParentSize()) {
+            drawCircle(
+                color = Color.Blue,
+                radius = size.minDimension / 2
+            )
+        }
+        Text(
+            text = letter,
+            color = Color.White,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
